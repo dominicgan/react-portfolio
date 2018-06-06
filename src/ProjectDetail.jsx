d60f99079d89c0968a531a44472a9a2aee7dcc75
dominicgangx <dominicgangx@gmail.com>
2018-06-06 12:51:11 +0800

    fix(project-detail): styling changes

--- src/ProjectDetail.jsx
+++ src/ProjectDetail.jsx
@@ -42,93 +42,95 @@ class ProjectDetail extends Component {
   				return;
   			})
   	}
-	render() {
-		let printCategories = () => {
-			if (this.state.project.categories) {
-				let outputObj = this.state.project.categories.map((el,i) => {
-					return (
-						<li key={i}>
-						<span>{el}</span>
-						</li>
-						)
-				});
-
-				return outputObj;
-			}
-		};
-
-		let printImages = () => {
-			if (this.state.project.images) {
-				let outputObj = this.state.project.images.map((el,i) => {
-					return (
-						<li key={i}>
-							<picture>
-								<source srcSet={this.state.baseUrl + el.sm} media="(min-width: 768px)"/>
-								<source srcSet={this.state.baseUrl + el.md} media="(min-width: 991pxpx)"/>
-								<source srcSet={this.state.baseUrl + el.lg} media="(min-width: 1200px)"/>
-								<source srcSet={this.state.baseUrl + el.src} media="(min-width: 1680px)"/>
-								<img width='150' src={this.state.baseUrl + el.xs} alt={this.state.project.title}/>
-							</picture>
-						</li>
-						)
-				});
-
-				return outputObj;
-			}
-		};
-
-		let printCoverImage = (obj) => {
-			if (obj) {
+	renderCategories() {
+		if (this.state.project.categories) {
+			let outputObj = this.state.project.categories.map((el,i) => {
 				return (
-					<picture>
-						<source srcSet={this.state.baseUrl + obj.sm} media="(min-width: 768px)"/>
-						<source srcSet={this.state.baseUrl + obj.md} media="(min-width: 991pxpx)"/>
-						<source srcSet={this.state.baseUrl + obj.lg} media="(min-width: 1200px)"/>
-						<source srcSet={this.state.baseUrl + obj.src} media="(min-width: 1680px)"/>
-						<img width='150' src={this.state.baseUrl + obj.xs} alt={this.state.project.title}/>
-					</picture>
+					<li key={i}>
+					<span>{el}</span>
+					</li>
 					)
-			}
-		};
+			});
 
-		let printProjectContent = () => {
-			if (this.state.project.content) {
-				return {__html: this.state.project.content};
-			}
+			return outputObj;
 		}
+	}
 
-	    return this.state.isLoading ?
-	    	(<div><h1>Loading</h1></div>) :
-	    	(
-			  <div className="project-detail">
-				<h1>Detail</h1>
-				<h2>{this.props.match.params.id}</h2>
-				<article>
-					<header>
-						<h2>{this.state.project.title}</h2>
-						<br/>
-						<span>{this.state.project.attribution}</span>
-						<br/>
-						<span>{this.state.project.client}</span>
-					</header>
-					<br/>
-					<span>{printCoverImage(this.state.project.coverImage)}</span>
-					<br/>
-					<span>{printCoverImage(this.state.project.coverImageIndex)}</span>
-					<br/>
-					<time dateTime={this.state.project.date}>{this.state.project.date}</time>
-					<br/>
-					<a href={this.state.project.link} target='_blank' rel='noopener noreferrer'>Visit Site</a>
-					<ul>{printCategories()}</ul>
-					<br/>
-					<ul>{printImages()}</ul>
-					<main dangerouslySetInnerHTML={printProjectContent()}></main>
-					<hr/>
-				</article>
-				<Link to='/projects'>Back to projects</Link>
-			  </div>
+	renderImages() {
+		if (this.state.project.images) {
+			let outputObj = this.state.project.images.map((el,i) => {
+				return (
+					<li key={i}>
+						<picture>
+							<source srcSet={this.state.baseUrl + el.sm} media="(min-width: 768px)"/>
+							<source srcSet={this.state.baseUrl + el.md} media="(min-width: 991pxpx)"/>
+							<source srcSet={this.state.baseUrl + el.lg} media="(min-width: 1200px)"/>
+							<source srcSet={this.state.baseUrl + el.src} media="(min-width: 1680px)"/>
+							<img width='150' src={this.state.baseUrl + el.xs} alt={this.state.project.title}/>
+						</picture>
+					</li>
+					)
+			});
+
+			return outputObj;
+		}
+	}
+
+	renderCoverImage(obj) {
+		if (obj) {
+			return (
+				<picture>
+					<source srcSet={this.state.baseUrl + obj.sm} media="(min-width: 768px)"/>
+					<source srcSet={this.state.baseUrl + obj.md} media="(min-width: 991pxpx)"/>
+					<source srcSet={this.state.baseUrl + obj.lg} media="(min-width: 1200px)"/>
+					<source srcSet={this.state.baseUrl + obj.src} media="(min-width: 1680px)"/>
+					<img width='150' src={this.state.baseUrl + obj.xs} alt={this.state.project.title}/>
+				</picture>
+				)
+		}
+	}
+
+	renderProjectContent() {
+		if (this.state.project.content) {
+			return {__html: this.state.project.content};
+		}
+	}
+	render() {
+
+	    if (this.state.isLoading) {
+	    	return (
+	    		<div><h1>Loading</h1></div>
+	    		)
+	    } else {
+	    	return (
+	    		<div className="project-detail">
+		    		<h1>Detail</h1>
+		    		<article>
+			    		<header>
+				    		<h2>{this.state.project.title}</h2>
+				    		<ul>
+				    		<li>{this.state.project.attribution}</li>
+				    		<li>{this.state.project.client}</li>
+				    		</ul>
+			    		</header>
+			    		<ul>
+				    		<li>{this.renderCoverImage(this.state.project.coverImage)}</li>
+				    		<li>{this.renderCoverImage(this.state.project.coverImageIndex)}</li>
+			    		</ul>
+			    		<ul>
+				    		<li><time dateTime={this.state.project.date}>{this.state.project.date}</time></li>
+				    		<li><a href={this.state.project.link} target='_blank' rel='noopener noreferrer'>Visit Site</a></li>
+			    		</ul>
+			    		<ul>{this.renderCategories()}</ul>
+			    		<ul>{this.renderImages()}</ul>
+			    		<main dangerouslySetInnerHTML={this.renderProjectContent()}></main>
+			    		<hr/>
+		    		</article>
+		    		<Link to='/projects'>Back to projects</Link>
+	    		</div>
 		    );
 				// <code>{JSON.stringify(this.state.project)}</code>
+	  }
   }
 }
 
